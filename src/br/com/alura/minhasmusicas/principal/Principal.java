dc3875fa0240eb4d9503eade7778e2ebcae0183f
João Medeiros <joaomanoelmedeirosmatos@gmail.com>
2025-10-22 20:25:56 -0300

    Add files via upload

---
+++ src/br/com/alura/minhasmusicas/principal/Principal.java
@@ -0,0 +1,37 @@
+package br.com.alura.minhasmusicas.principal;
+
+import br.com.alura.minhasmusicas.modelos.MinhasPreferidas;
+import br.com.alura.minhasmusicas.modelos.Musicas;
+import br.com.alura.minhasmusicas.modelos.PodCast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musicas minhaMusica = new Musicas();
+        minhaMusica.setTitulo("Musica de amor nunca mais");
+        minhaMusica.setCantor("BK");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        PodCast meuPodcast = new PodCast();
+        meuPodcast.setTitulo("Bolha dev");
+        meuPodcast.setApresentador("Marcos mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
+}
